ef9bd980d40678613dd25c450997c771120b2da7
Erin Meredith <ekmeredith@att.net>
2019-02-18 19:22:17 -0500

    completed demo code; added xml to tablet layout

--- app/src/main/java/com/lambdaschool/swapi/MainActivity.java
+++ app/src/main/java/com/lambdaschool/swapi/MainActivity.java
@@ -8,17 +8,26 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Trace;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
 import android.transition.Fade;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SwApiListFragment.OnListFragmentInteractionListener, CategoriesFragment.OnCategoryFragmentInteractionListener {
 
     Context context;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,40 @@ public class MainActivity extends AppCompatActivity implements SwApiListFragment
 
         setContentView(R.layout.activity_main);
         context = this;
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(context, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                menuItem.setChecked(!menuItem.isChecked());
+
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_planets:
+                        onFragmentInteraction(SwApiListFragment.CATEGORY_PLANET);
+                        break;
+                    case R.id.nav_vehicles:
+                        onFragmentInteraction(SwApiListFragment.CATEGORY_TRANSPORT);
+                        break;
+                    case R.id.nav_starships:
+                        onFragmentInteraction(SwApiListFragment.CATEGORY_TRANSPORT);
+                        break;
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
 
         Trace.beginSection("Adding Fragment");
         Fragment fragment = new CategoriesFragment();
